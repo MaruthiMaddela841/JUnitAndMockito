e7dc82ce42eb28780e77f5569fc876a13032015e
Maruthi Maddela <maruthimaddela841@gmail.com>
2025-05-28 19:21:38 +0530

    latest

--- JunitProject_2/src/in/ineuron/service/BankLoanService.java
+++ JunitProject_2/src/in/ineuron/service/BankLoanService.java
@@ -4,7 +4,7 @@ public class BankLoanService {
 	public float calculateSimpleInterest(float pAmount,float rate,float time) {
 		System.out.println("BankLoanService.calculateSimpleInterest()");
 		if (pAmount<=0 || rate<=0 || time<=0)
-			throw new IllegalArgumentException("Invliad inputs");
+			throw new IllegalArgumentException("Invalid inputs");
 		return (pAmount*rate*time)/100.0f;
 	}	
 }
