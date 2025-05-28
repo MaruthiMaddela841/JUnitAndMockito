a2485a6ba06de4731954c0713bd66624f4706ecc
Maruthi Maddela <maruthimaddela841@gmail.com>
2025-05-28 23:19:02 +0530

    latest

---
+++ 01_Spring_MongoDB/src/main/java/in/ineuron/Application.java
@@ -0,0 +1,17 @@
+package in.ineuron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@SpringBootApplication
+@EnableMongoRepositories("in.ineuron.repository")
+@ComponentScan("in.ineuron.*")
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
