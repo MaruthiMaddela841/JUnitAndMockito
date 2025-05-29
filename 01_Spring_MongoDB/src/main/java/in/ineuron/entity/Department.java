ee2f93f0093d96e4f07665fb0b758bf9b4709dd7
Maruthi Maddela <maruthimaddela841@gmail.com>
2025-05-29 18:23:00 +0530

    latest

--- 01_Spring_MongoDB/src/main/java/in/ineuron/entity/Department.java
+++ 01_Spring_MongoDB/src/main/java/in/ineuron/entity/Department.java
@@ -1,8 +1,14 @@
 package in.ineuron.entity;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
+//@Document(collection = "department")
 public class Department {
+	
+//	@Id
+//	private String id;
 
 	@Field(name = "department_name")
 	private String departmentName;
--- 01_Spring_MongoDB/src/main/java/in/ineuron/entity/Subject.java
+++ 01_Spring_MongoDB/src/main/java/in/ineuron/entity/Subject.java
@@ -1,8 +1,14 @@
 package in.ineuron.entity;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
+//@Document(collection = "subject")
 public class Subject {
+	
+//	@Id
+//	private String id;
 
 	@Field(name = "subject_name")
 	private String subjectName;
